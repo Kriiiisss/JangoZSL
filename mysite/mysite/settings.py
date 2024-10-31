59c083fcaf6c76975264705e37b5f2bedc7df533
Kriiiisss <krzysinek06@gmail.com>
2024-10-31 07:45:13 +0100

    3

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2^=t+c7gt5z&tvr7l34q(b7xvfnt*l-bi&kq7+0os6k-0##@p%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
